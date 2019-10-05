a6c6afe48a88b0e53ccb72d095217775962358ed
pyons <broad.basic15@gmail.com>
2019-10-05 12:23:17 +0900

    use introspections.

--- Tuesday/dynamic_dispatch.rb
+++ Tuesday/dynamic_dispatch.rb
@@ -1,26 +1,13 @@
 class Computer
-  def initialize(computer_id, data_source)
-    @id = computer_id
-    @data_source = data_source
-  end
 
-  def mouse
-    component :mouse
+  def self.define_component(name)
+    define_method(name) do 
+      info = @data_source.send "get_#{name}_info", @id
+      price = @data_source.send "get_#{name}_price", @id
+      result = "#{name.capitalize}: #{info}  ($#{price})"
+      return result if price >= 100
+      result
+    end
   end
-
-  def cpu 
-    component :cpu
-  end
-
-  def keybord
-    component :keybord
-  end
-
-  def component(name)
-    info = @data_source.send "get_#{name}_info", @id
-    price = @data_source.send "get_#{name}_price", @id
-    result = "#{name.capitalize}: #{info}  ($#{price})"
-    return result if price >= 100
-    result
-  end
+ 
 end
